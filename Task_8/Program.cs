16c2d696d94965329e3d61907e999b056596e7ce
Oleg <legyss@mail.ru>
2023-01-15 06:20:21 +0600

    modified Task_8

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,19 +1,29 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N.
 
-Console.WriteLine("Введите положительное число (N) большее 1: ");
+Console.WriteLine("Введите число (N) отличное от 1: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Четные числа от 1 до N: ");
 if (number > 1)
 {
-    Console.Write("Четные числа от 1 до N: ");
-    int count = 2;
+    int count = 1;
     while (count <= number)
     {
         if (count % 2 == 0)
             Console.Write($"{count}, ");
         count++;
     }
-    Console.Write("\b\b ");
 }
-else Console.Write("Ошибка!Введенное число должно быть больше 1.");
+else
+{
+    int count = 1;
+    while (count >= number)
+    {
+        if (count % 2 == 0)
+            Console.Write($"{count}, ");
+        count--;
+    }
+}
+Console.Write("\b\b ");
+
